ae81aa13188a2c1d6123f0b4b81c5930fb214aec
lucas-Palladino-Gonzalez <lpalladinogonzalez@frba.utn.edu.ar>
2022-04-19 17:02:18 -0300

    Uploading main.c file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int caracter;
+    do{
+        caracter = getchar();
+        if (isdigit(caracter))
+        {
+            continue;
+        }   else if(isupper(caracter))
+            {
+             caracter = tolower(caracter);
+            }   else if(islower(caracter))
+                {
+                    caracter = toupper(caracter);
+                }
+        putchar(caracter);
+    } while (caracter != EOF);
+    return 0;
+}
